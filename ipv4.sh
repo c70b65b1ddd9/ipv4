460adedfcd80381f4b8a4b49bd55613d6f4c635e
c70b65b1ddd9 <168281329+c70b65b1ddd9@users.noreply.github.com>
2024-06-30 18:32:31 +0300

    Update ipv4.sh

--- ipv4.sh
+++ ipv4.sh
@@ -8,8 +8,6 @@ SIFRE=kurek
 
 #------------------#
 IPV4_PORT=3310
-
-SOCKS5_PORT=5110
 #------------------#
 
 #------------------#
@@ -23,85 +21,37 @@ sari='\e[1;93m'
 mor='\e[0;35m'
 #------------------#
 
-yukle_3proxy() {
-    echo -e "\n\n\t$yesil 3Proxy Yükleniyor..\n$renkreset\n"
-    URL=https://github.com/c70b65b1ddd9/ipv4/raw/main/Paketler/3proxy-0.9.4.tar.gz
-    wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-0.9.4.tar.gz
-    make -f Makefile.Linux
-    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp -f src/3proxy /usr/local/etc/3proxy/bin/
-    cp -f ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
-    cd ..
-    rm -rf 3proxy-3proxy-0.8.6
-    cd $YOL
-}
-
 rastgele() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
 veri_olustur() {
-    seq 10000 10100 | while read port; do
-        echo ${KULLANICI}$(rastgele)/${SIFRE}$(rastgele)/$IP4/$port
+    for ip in 5.180.155.17 5.180.155.220 5.180.155.227 5.180.155.228 5.180.155.244 5.180.155.248; do
+        echo ${KULLANICI}$(rastgele)/${SIFRE}$(rastgele)/$ip/$IPV4_PORT
     done
-}
-
-iptable_olustur() {
-    cat <<EOF
-    $(awk -F / '{print iptables -I INPUT -p tcp --dport  $4   -m state --state NEW -j ACCEPT}' ${VERI})
-EOF
-}
-
-config_3proxy() {
-    cat <<EOF
-daemon
-maxconn 1000
-nscache 65536
-timeouts 1 5 30 60 180 1800 15 60
-setgid 65535
-setuid 65535
-flush
-auth strong
-
-users $(awk -F / 'BEGIN{ORS=;} {print $1 :CL: $2  }' ${VERI})
-
-$(awk -F / '{print auth strong\n \
-allow  $1 \n \
-proxy -n -a -p $4  -i $3\n \
-flush\n}' ${VERI})
-EOF
 }
 
 squid_yukle() {
     echo -e "\n\n\t$yesil Squid Yükleniyor..\n$renkreset\n"
-    yum install nano dos2unix squid httpd-tools -y
+    yum install nano dos2unix squid httpd-tools -y      # >/dev/null
     htpasswd -bc /etc/squid/passwd $KULLANICI $SIFRE
 
     cat >/etc/squid/squid.conf <<EOF
 auth_param basic program /usr/lib64/squid/basic_ncsa_auth /etc/squid/passwd
 auth_param basic realm proxy
 acl authenticated proxy_auth REQUIRED
-acl smtp port 25
 http_access allow authenticated
 
 http_port 0.0.0.0:${IPV4_PORT}
-
-http_access deny smtp
-http_access deny all
 forwarded_for delete
+http_access deny all
 EOF
 
-    cp -f /etc/squid/squid.conf /etc/init.d/squid
-    touch /etc/squid/blacklist.acl
     systemctl restart squid.service && systemctl enable squid.service
 
     iptables -I INPUT -p tcp --dport $IPV4_PORT -j ACCEPT
-
-    iptables-save
+    iptables-save                                      # >/dev/null
 }
 
 proxy_txt() {
@@ -120,37 +70,21 @@ file_io_yukle() {
     echo -e "\n\n\t$yesil Zip Yükleniyor..\n$renkreset\n"
 
     local PASS=$(rastgele)
-    zip --password $PASS proxy.zip proxy.txt
+    zip --password $PASS proxy.zip proxy.txt           # -qq
     JSON=$(curl -sF file=@proxy.zip https://file.io)
     URL=$(echo $JSON | jq --raw-output '.link')
 
     clear
     echo -e "\n\n\t$yesil Proxyler Hazır!$mor Format »$sari :$renkreset"
-    echo -e "\n$mor IPv6 Zip İndirme Bağlantısı:$yesil ${URL}$renkreset"
-    echo -e "$mor IPv6 Zip Şifresi:$yesil ${PASS}$renkreset"
+    echo -e "\n$mor IPv4 Zip İndirme Bağlantısı:$yesil ${URL}$renkreset"
+    echo -e "$mor IPv4 Zip Şifresi:$yesil ${PASS}$renkreset"
 }
 
-socks5_yukle() {
-    echo -e "\n\n\t$yesil Dante SOCKS5 Yükleniyor..\n$renkreset\n"
-
-    wget -qO dante_socks.sh https://raw.githubusercontent.com/Lozy/danted/master/install_centos.sh
-    chmod +x dante_socks.sh
-    ./dante_socks.sh --port=$SOCKS5_PORT --user=$KULLANICI --passwd=$SIFRE
-    rm -rf dante_socks.sh
-
-    iptables -I INPUT -p tcp --dport $SOCKS5_PORT -j ACCEPT
-    iptables-save
-}
-
-IP4=$(curl -4 -s icanhazip.com)
-VERI=proxy-veri.txt
-YOL=$(pwd)
-
-veri_olustur >$VERI
-iptable_olustur
-ifconfig_olustur
-config_3proxy > /usr/local/etc/3proxy/3proxy.cfg
-
-yukle_3proxy
+# Main
+VERI=$(mktemp)
+veri_olustur >${VERI}
 squid_yukle
-proxy_txt && jq_yukle && file_io_yukle
+proxy_txt
+jq_yukle
+file_io_yukle
+rm -f ${VERI}
